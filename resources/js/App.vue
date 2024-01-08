6961450b75b0b589394a2544b073b573704f7c88
Atif Ibrahim <dedezigners@gmail.com>
2024-01-09 04:03:23 +0500

    init laravel & vue template

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="main">
+        <header class="main__header">
+            <h2>Kanban Board</h2>
+        </header>
+
+        <div class="main__content"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainApp",
+    data() {
+        return {}
+    },
+    computed: {},
+    methods: {}
+}
+</script>
